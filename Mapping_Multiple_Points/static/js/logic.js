b5ff774681a61aa4034cd9ff8fe6b7acdcca1a4a
Jackson Sheppard <sheppard@slac.stanford.edu>
2021-02-28 14:13:34 -0800

    MNT: one more comment delete

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -2,7 +2,6 @@
 console.log("working");
 
 // Create the map object with a center and zoom level
-// let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // Get data from cities.js
